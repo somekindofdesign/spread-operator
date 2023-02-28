a34511fda4c3faf4bbb01682f8f2e5d3fc682c9a
Louise Stanley <somekindofdesign@gmail.com>
2023-02-28 11:00:06 +0000

    Lesson

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,8 +4,21 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+
+arr2.push(4);
+
+console.log("Second array:", arr2)
+console.log("First array:", arr1)
 
 // Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+
+arr4.push(7);
+console.log("Fourth array:", arr4)
+console.log("Third array:", arr3)
 
 // Copying an object
 
